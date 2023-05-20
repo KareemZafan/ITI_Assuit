f39745fc04c28e3750b69bb0bd37804c18e39f8f
shroukAbdelsalam <shroukmabdelsalam@gmail.com>
2023-05-20 11:33:19 +0300

    add new Name Shrouk in main
    my edit to the main
    
    add thanks
    
    Revert "add thanks"
    
    This reverts commit e0c8ebbab7cba648a9edfcd9b4dc8c7b93fc610e.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> names = new ArrayList<>(List.of("Mostafa","Khaled","Omar"));
-
+        List<String> names = new ArrayList<>(List.of("Mostafa","Khaled","Omar","Shrouk", "Mahmoud"));
         for(String name:names){
             System.out.println(name);
         }
