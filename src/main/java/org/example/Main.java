8588fe8bac20123fc2669893c616e397cdcafad3
KareemZafan <kareemm.zafan@gmail.com>
2023-05-20 11:18:43 +0300

    Adding our names

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>(List.of("Mostafa","Khaled","Omar"));
+
+        for(String name:names){
+            System.out.println(name);
+        }
+    }
+}
