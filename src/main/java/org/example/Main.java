d5fdce306d5524849ec66d37cc2448a36675bfe9
shroukAbdelsalam <shroukmabdelsalam@gmail.com>
2023-05-20 11:33:19 +0300

    add new Name Shrouk in main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> names = new ArrayList<>(List.of("Mostafa","Khaled","Omar"));
+        List<String> names = new ArrayList<>(List.of("Mostafa","Khaled","Omar","Shrouk"));
 
         for(String name:names){
             System.out.println(name);
